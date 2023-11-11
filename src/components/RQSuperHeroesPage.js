ef591783c067c1e297a6491f9e654d3a5b97dd7b
dat.truong <dat.truong@secomm.vn>
2023-11-11 21:19:53 +0700

    fetching data with useQuery

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -1,5 +1,27 @@
-import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 export const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroesPage</div>;
+  const { isLoading, data, ...rest } = useQuery("super-hero", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(rest);
+
+  return (
+    <div>
+      <h2>RQSuperHeroesPage</h2>
+      <div>
+        {data?.data.map((item) => {
+          return <div key={item.id}>{item.name}</div>;
+        })}
+      </div>
+    </div>
+  );
 };
