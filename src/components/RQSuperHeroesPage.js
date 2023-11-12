c157514553596dc8743bacb44b9e130b2571d0bb
dat.truong <dat.truong@secomm.vn>
2023-11-12 14:45:58 +0700

    Handle Query error

--- src/components/RQSuperHeroesPage.js
+++ src/components/RQSuperHeroesPage.js
@@ -2,17 +2,22 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, ...rest } = useQuery("super-hero", fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    "super-hero",
+    fetchSuperHeroes
+  );
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  console.log(rest);
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
 
   return (
     <div>
