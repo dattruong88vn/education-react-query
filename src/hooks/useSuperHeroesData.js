d9f22516e2316c57609c5a18b009117c3ee35ea7
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-11-14 22:31:22 +0700

    update before mutate: revert and refetch

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -23,17 +23,50 @@ export const useSuperHeroesData = ({ onSuccess, onError, options = {} }) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // cách 1: refetch api get list super-hero after create new one
-      // queryClient.invalidateQueries("super-heroes");
-
-      // cách 2: sử dụng response sau khi mutate thành công và thêm vào cache data của list super hero
+    // onSuccess: (data) => {
+    //   // cách 1: refetch api get list super-hero after create new one
+    //   // queryClient.invalidateQueries("super-heroes");
+    //   // cách 2: sử dụng response sau khi mutate thành công và thêm vào cache data của list super hero
+    //   queryClient.setQueriesData("super-heroes", (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
+    // cách 3: update data ở client trước khi mutate
+    /**
+     * onMutate --> call trước khi mutate api
+     * Tham số là data được gửi lên mutate
+     * Trả về previousData
+     */
+    onMutate: (newHero) => {
+      const previousData = queryClient.getQueriesData("super-heroes");
       queryClient.setQueriesData("super-heroes", (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+
+      return {
+        ...previousData,
+      };
+    },
+    /**
+     * onError --> call sau mutate api lỗi
+     */
+    onError: (_error, _hero, context) => {
+      queryClient.setQueriesData("super-heroes", context.previousData);
+    },
+    /**
+     * onSettled --> call sau mutate api hoàn thành (thành công hoặc lỗi)
+     */
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes");
     },
   });
 };
