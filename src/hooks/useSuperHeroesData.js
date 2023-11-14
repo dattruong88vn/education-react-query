ffab3a2022e18a46ec72bfc159c39b53cc9ff5f4
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-11-14 22:15:53 +0700

    Handle Mutation response to update ui

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -23,8 +23,17 @@ export const useSuperHeroesData = ({ onSuccess, onError, options = {} }) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: () => {
-      queryClient.invalidateQueries("super-heroes");
+    onSuccess: (data) => {
+      // cách 1: refetch api get list super-hero after create new one
+      // queryClient.invalidateQueries("super-heroes");
+
+      // cách 2: sử dụng response sau khi mutate thành công và thêm vào cache data của list super hero
+      queryClient.setQueriesData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, data.data],
+        };
+      });
     },
   });
 };
